416f7b08407e0736735f1374aa7d1549542d514d
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2020-11-30 20:17:47 +0000

    Fork from CircuitProcessing 0.3.5

---
+++ KiwiHawksCircuitProcessing/data-updates.lua
@@ -0,0 +1,15 @@
+require "bobelectronics"
+require "bobmodules"
+
+data.raw.recipe['rocket-control-unit'].normal = nil
+data.raw.recipe['rocket-control-unit'].expensive = nil
+local module = 'speed-module'
+if data.raw.module['speed-module-8'] then
+  module = 'speed-module-2'
+end
+data.raw.recipe['rocket-control-unit'].ingredients = {
+  {"advanced-processing-unit", 1},
+  {module, 1}
+}
+data.raw.recipe['rocket-control-unit'].result = 'rocket-control-unit'
+data.raw.recipe['rocket-control-unit'].energy_required = 30
