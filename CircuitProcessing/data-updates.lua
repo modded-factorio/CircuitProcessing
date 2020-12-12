f3c3487b93cda243fac508e479b75982d383e46f
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2020-12-12 23:37:53 +0000

    Hiding Agriculture Modules for now
    Modules are still being changed by Angel's devs. Revisit this when I have more time and they are stable and feature complete.

--- CircuitProcessing/data-updates.lua
+++ CircuitProcessing/data-updates.lua
@@ -13,3 +13,23 @@ data.raw.recipe['rocket-control-unit'].ingredients = {
 }
 data.raw.recipe['rocket-control-unit'].result = 'rocket-control-unit'
 data.raw.recipe['rocket-control-unit'].energy_required = 30
+
+-- Hide Agriculture Modules. Revisit later, once Angel's devs have finished their changes
+local function disable(name)
+  if data.raw.technology[name] then
+    data.raw.technology[name].enabled = false
+    data.raw.technology[name].hidden = true
+  end
+  if data.raw.recipe[name] then
+    data.raw.recipe[name].hidden = true
+  end
+  if data.raw.module[name] then
+    data.raw.module[name].flags = {'hidden'}
+  end
+end
+for _,v in pairs({
+  'angels-bio-yield-module',
+  'angels-bio-yield-module-2',
+  'angels-bio-yield-module-3'}) do
+  disable(v)
+end
