aca9abfc93aba1cb23590d2652151640b6d14741
Athena Faris <75452942+KompetenzAirbag@users.noreply.github.com>
2025-10-19 19:16:24 +0200

    Fixed prerequisites for angels-bio-modules

--- CircuitProcessing/angelsmodules.lua
+++ CircuitProcessing/angelsmodules.lua
@@ -6,3 +6,15 @@ lib.replace_recipe_ingredient("angels-bio-yield-module-2", "bob-pollution-create
 lib.replace_recipe_ingredient("angels-bio-yield-module-3", "bob-pollution-create-module-3", "efficiency-module-3")
 lib.replace_recipe_ingredient("angels-bio-yield-module-4", "bob-pollution-create-module-4", "bob-efficiency-module-4")
 lib.replace_recipe_ingredient("angels-bio-yield-module-5", "bob-pollution-create-module-5", "bob-efficiency-module-5")
+
+bobmods.lib.tech.remove_prerequisite("angels-bio-yield-module", "bob-pollution-create-module-1")
+bobmods.lib.tech.remove_prerequisite("angels-bio-yield-module-2", "bob-pollution-create-module-2")
+bobmods.lib.tech.remove_prerequisite("angels-bio-yield-module-3", "bob-pollution-create-module-3")
+bobmods.lib.tech.remove_prerequisite("angels-bio-yield-module-4", "bob-pollution-create-module-4")
+bobmods.lib.tech.remove_prerequisite("angels-bio-yield-module-5", "bob-pollution-create-module-5")
+
+bobmods.lib.tech.add_prerequisite("angels-bio-yield-module", "efficiency-module")
+bobmods.lib.tech.add_prerequisite("angels-bio-yield-module-2", "efficiency-module-2")
+bobmods.lib.tech.add_prerequisite("angels-bio-yield-module-3", "efficiency-module-3")
+bobmods.lib.tech.add_prerequisite("angels-bio-yield-module-4", "bob-efficiency-module-4")
+bobmods.lib.tech.add_prerequisite("angels-bio-yield-module-5", "bob-efficiency-module-5")
